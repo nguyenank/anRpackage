796c9558d68b9198024770c1b65943f035a1ec6e
An <anknguyenanknguyen@gmail.com>
2022-04-13 16:44:56 -0700

    :sparkles: add axes toggle

--- R/theme_an.R
+++ R/theme_an.R
@@ -1,23 +1,46 @@
 #' Personal theme.
 #'
+#' @param axes whether to include axes or not
+#'
 #' @return ggplot2 theme object
 #' @export
 #'
 #' @examples
 #' theme_an()
-theme_an <- function() {
-
+theme_an <- function(axes = TRUE) {
   sysfonts::font_add_google("Public Sans", "public_sans")
   showtext::showtext_auto()
 
-  ggplot2::theme_void() +
-  ggplot2::theme(
-    text = ggplot2::element_text(family = "public_sans"),
-    plot.title = ggplot2::element_text(face = "bold", hjust = 0.5),
-    plot.title.position = "plot",
-    plot.subtitle = ggplot2::element_text(hjust = 0.5),
-    plot.caption = ggplot2::element_text(face = "italic", size = 9, hjust = 0.5),
-    plot.caption.position = "plot",
-    legend.title = ggplot2::element_text(face = "bold"),
-  )
+  base <- ggplot2::theme_void() +
+    ggplot2::theme(
+      text = ggplot2::element_text(family = "public_sans"),
+      plot.title = ggplot2::element_text(face = "bold", hjust = 0.5),
+      plot.title.position = "plot",
+      plot.subtitle = ggplot2::element_text(hjust = 0.5),
+      plot.caption = ggplot2::element_text(
+        face = "italic",
+        size = 9,
+        hjust = 0.5,
+        margin = margin(t = 1.5, r = 0, b = 0, l = 0, "char")
+      ),
+      plot.caption.position = "plot",
+      plot.margin = margin(t = 1.5, r = 1.5, b = 1.5, l = 1.5, "char"),
+      legend.title = ggplot2::element_text(face = "bold"),
+    )
+
+  if (axes) {
+    base <- base +
+      ggplot2::theme(
+        axis.line = ggplot2::element_line(color = "#555555", lineend = "round"),
+        axis.text.x = ggplot2::element_text(margin = margin(t = 0.3, unit = "char")),
+        axis.text.y = ggplot2::element_text(margin = margin(r = 0.3, unit = "char")),
+        axis.title = ggplot2::element_text(face = "bold"),
+        axis.ticks = ggplot2::element_line(color = "#555555"),
+        axis.ticks.length = ggplot2::unit(0.35, "char"),
+        axis.title.y = ggplot2::element_text(angle = 90, margin = margin(r = 0.75, unit = "char")),
+        axis.title.x = ggplot2::element_text(margin = margin(t = 0.75, unit = "char")),
+      )
+  }
+
+  base
 }
--- dev/experiment.R
+++ dev/experiment.R
@@ -4,3 +4,4 @@ ggplot(faithful) +
   geom_point(aes(x = eruptions, y = waiting), color = an_colors$orange) +
   labs(title = "This is my Title", subtitle = "we love subtitles", caption = "source: R defaults? your friendly local volcano?") +
   theme_an()
+
