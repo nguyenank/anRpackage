656baf9643474f08ae9f665d551273b9dbf1e5b6
An <anknguyenanknguyen@gmail.com>
2022-04-13 15:41:35 -0700

    :sparkles: add an_colors

---
+++ R/an_colors.R
@@ -0,0 +1,10 @@
+#' A list of colors I want regular access to.
+#' @export an_colors
+#' @examples
+#' an_colors$blue
+an_colors <- list("blue" = "#3bbfbd",
+                  "orange" = "#eaa169",
+                  "grey" = "#aaaaaa")
+
+
+
